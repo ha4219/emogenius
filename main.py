353206e44f5592da2d8b69558a5a6ec54a2fe14a
ha4219 <ha4219@naver.com>
2022-05-23 15:54:32 +0900

    init

--- main.py
+++ main.py
@@ -1,13 +1,152 @@
-import mxnet
+from copy import deepcopy
+from statistics import mode
+from time import time
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
 import gluonnlp as nlp
-import pandas as pd
+import numpy as np
+from tqdm import tqdm
+#kobert
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
 
-TRAIN_PATH = '/home/dongha/code/data/conversation/Training/final_Training.xlsx'
+#transformers
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
 
-# Index(['번호', '연령', '성별', '상황키워드', '신체질환', '감정_대분류', '감정_소분류', '사람문장1',
-#  '시스템응답1', '사람문장2', '시스템응답2', '사람문장3', '시스템응답3', '사람문장4', '시스템응답4'],
-# dtype='object')
-data = pd.read_excel(TRAIN_PATH)
-for i in data['감정_소분류']:
-  print(i)
+from dataset import CustomDataset
+from preprocessing import preprocessing
+#GPU 사용
+device = torch.device("cuda:0")
+#BERT 모델, Vocabulary 불러오기
+bertmodel, vocab = get_pytorch_kobert_model()
 
+# Setting parameters
+max_len = 64
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 50
+max_grad_norm = 1
+log_interval = 200
+learning_rate =  5e-5
+
+#토큰화
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+#train & test 데이터로 나누기
+from sklearn.model_selection import train_test_split
+
+data = preprocessing()
+
+dataset_train, dataset_test = train_test_split(data, test_size=0.25, random_state=0)
+
+data_train = CustomDataset(dataset_train, 0, 1, tok, max_len, True, False)
+data_test = CustomDataset(dataset_test, 0, 1, tok, max_len, True, False)
+
+train_dataloader = torch.utils.data.DataLoader(data_train, batch_size=batch_size, num_workers=5)
+test_dataloader = torch.utils.data.DataLoader(data_test, batch_size=batch_size, num_workers=5)
+
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes=58,   ##클래스 수 조정##
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+                 
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+    
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+
+  #BERT 모델 불러오기
+model = BERTClassifier(bertmodel,  dr_rate=0.5).to(device)
+
+#optimizer와 schedule 설정
+no_decay = ['bias', 'LayerNorm.weight']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+]
+
+optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
+loss_fn = nn.CrossEntropyLoss()
+
+t_total = len(train_dataloader) * num_epochs
+warmup_step = int(t_total * warmup_ratio)
+
+scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
+
+#정확도 측정을 위한 함수 정의
+def calc_accuracy(X,Y):
+  max_vals, max_indices = torch.max(X, 1)
+  train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
+  return train_acc
+    
+best_acc = 0
+best_model = deepcopy(model.state_dict())
+
+since = time()
+
+for e in range(num_epochs):
+  train_acc = 0.0
+  test_acc = 0.0
+  model.train()
+  for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm(train_dataloader)):
+    optimizer.zero_grad()
+    token_ids = token_ids.long().to(device)
+    segment_ids = segment_ids.long().to(device)
+    valid_length= valid_length
+    label = label.long().to(device)
+    out = model(token_ids, valid_length, segment_ids)
+    loss = loss_fn(out, label)
+    loss.backward()
+    torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm)
+    optimizer.step()
+    scheduler.step()  # Update learning rate schedule
+    train_acc += calc_accuracy(out, label)
+    # if batch_id % log_interval == 0:
+    #     print("epoch {} batch id {} loss {} train acc {}".format(e+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
+  print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
+  
+  model.eval()
+  for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm(test_dataloader)):
+    token_ids = token_ids.long().to(device)
+    segment_ids = segment_ids.long().to(device)
+    valid_length= valid_length
+    label = label.long().to(device)
+    out = model(token_ids, valid_length, segment_ids)
+    test_acc += calc_accuracy(out, label)
+  if test_acc > best_acc:
+    best_acc = test_acc
+    best_model = deepcopy(model.state_dict())
+  print("epoch {} test acc {}".format(e+1, test_acc / (batch_id+1)))
+
+
+time_elapsed = time() - since
+print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+print('Best val Acc: {:4f}'.format(best_acc))
+
+model.load_state_dict(best_model)
+torch.save(model, './model.pt')
---
+++ preprocessing.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+
+
+TRAIN_PATH = '/home/dongha/code/data/conversation/Training/final_Training.xlsx'
+
+def preprocessing(path=TRAIN_PATH):
+  data = pd.read_excel(path)
+  enc = LabelEncoder()
+  data['감정_소분류'] = enc.fit_transform(data['감정_소분류'])
+  
+  res = []
+  for q, label in zip(data['사람문장1'], data['감정_소분류']):
+    tmp = []
+    tmp.append(q)
+    tmp.append(label)
+    res.append(tmp)
+  return res
