b2656d1566d968674720a76a4480e294bcae2e29
ha4219 <ha4219@naver.com>
2022-05-23 12:13:32 +0900

    init

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ import pandas as pd
 
 TRAIN_PATH = '/home/dongha/code/data/conversation/Training/final_Training.xlsx'
 
+# Index(['번호', '연령', '성별', '상황키워드', '신체질환', '감정_대분류', '감정_소분류', '사람문장1',
+#  '시스템응답1', '사람문장2', '시스템응답2', '사람문장3', '시스템응답3', '사람문장4', '시스템응답4'],
+# dtype='object')
 data = pd.read_excel(TRAIN_PATH)
-print(data.sample(10))
+for i in data['감정_소분류']:
+  print(i)
+
